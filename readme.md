3f8092494b071995ad3b8c4f2736059b5c1d4a07
vladimirtudor22 <vladimirtudort@gmail.com>
2023-09-23 17:26:05 +0300

    First commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+## Runtime Environment Configuration
+
+To ensure smooth setup and functionality, follow these steps:
+
+1. **Install Google Chrome**: Ensure you have Google Chrome browser installed.
+
+2. **Run the following command to install necessary dependencies:**
+
+   ```bash
+   npm install --save-dev
+   ```
+
+3. **Run the following command to execute the tests:**
+   ```bash
+   npm test
+   ```
+
+Feel free to reach out if you have any questions or need further assistance! 🚀 Happy coding and best of luck with your project! ✨
---
+++ specs/SearchAProduct.test.js
@@ -0,0 +1,52 @@
+// https://demoqa.com/text-box
+import { Builder, By, Capabilities, Key } from "selenium-webdriver";
+import { describe, it } from "mocha";
+
+const capabilities = Capabilities.chrome();
+const driver = await new Builder().withCapabilities(capabilities).build();
+let title = "";
+describe("Search a product", () => {
+  it("Can open browser to TextBox", async () => {
+    await driver.manage().setTimeouts({ implicit: 10000 });
+    await driver.manage().window().setRect({ width: 1920, height: 1080 });
+    await driver.get("https://www.amazon.com");
+  });
+  it("Can type in searchBox 'forza horizon 5'", async () => {
+    await driver
+      .findElement(By.id("twotabsearchtextbox"))
+      .sendKeys("forza horizon 5");
+  });
+  it("Can press on search button", async () => {
+    await driver.findElement(By.id("nav-search-submit-button")).click();
+  });
+  it("Can see first product from search page", async () => {
+    await driver
+      .findElement(By.css('[data-cel-widget="search_result_3"]'))
+      .isDisplayed();
+  });
+  it("Can see desired result and get his text", async () => {
+    const text = await driver
+      .findElement(
+        By.xpath(
+          '//*[@id="search"]/div[1]/div[1]/div/span[1]/div[1]/div[4]/div/div/div/div/div/div[2]/div/div/div[1]/h2/a/span'
+        )
+      )
+      .getText();
+    title = text;
+  });
+  it("Can press on desired result", async () => {
+    const text = await driver
+      .findElement(
+        By.xpath(
+          '//*[@id="search"]/div[1]/div[1]/div/span[1]/div[1]/div[4]/div/div/div/div/div/div[2]/div/div/div[1]/h2/a/span'
+        )
+      )
+      .click();
+  });
+  it("DELAY", async () => {
+    await driver.sleep(8000);
+  });
+  it("Quit session", async () => {
+    await driver.quit();
+  });
+});
